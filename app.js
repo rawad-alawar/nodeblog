ca4f528e3a0152efcdce3bbfda17b022d144bfe6
Rawad Alawar <email": null>
2016-04-10 14:41:24 +1200

    sets up app.js

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator= require('express-validator')
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodeblog')
+
+var multer = require('multer');
+var flash = require('connect-flash')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,13 +21,54 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Handle File Uploads & Multipart Data
+app.use(multer({dest: './public/images/uploads'}))
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//Express session
+app.use(session({
+  secret:'secret',
+  saveUninitialized: true,
+  resave: true
+}))
+
+// Validator, formats errors
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Connect-flash, setting global variable called messages that we can access in any view
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Make our db accessible to our router
+app.use(function(req, res, next) {
+  req.db =  db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);

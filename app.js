0552df36a44dbd9753e244daa35f58731f4c7ff7
Rawad Alawar <email": null>
2016-04-10 20:18:45 +1200

    create form for adding posts

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ var multer = require('multer');
 var flash = require('connect-flash')
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
+
 
 var app = express();
+
+//Use app.locals to make a global variable
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +27,7 @@ app.set('view engine', 'jade');
 
 //Handle File Uploads & Multipart Data
 var multer = require('multer');
-var upload = multer({ dest: './uploads' });  
+var upload = multer({ dest: './uploads' });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -71,7 +75,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/posts.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodeblog')   //name of our db is localhost/nodeblog
+
+router.get('/add',function(req, res, next){
+  //to list categories in drop down
+  var categories = db.get('categories')
+  categories.find({},{},function(err, categories){
+
+  })
+
+  res.render('addpost',{
+    "title": "Add Post",
+    "categories": categories
+  })
+})
+
+router.post('/add', function(req, res, next){
+    // Ger form values
+    var title    = req.body.title;
+    var category = req.body.category;
+    var body     = req.body.body;
+    var author   = req.body.author;
+    var date     = new Date();
+
+    if(req.files.mainimage){
+        var mainImageOriginalName = req.files.mainimage.originalname;
+        var mainImageName         = req.files.mainimage.name;
+        var mainImageMime         = req.files.mainimage.mimetype;
+        var mainImagePath         = req.files.mainimage.path;
+        var mainImageExt          = req.files.mainimage.extension;
+        var mainImageSize         = req.files.mainimage.size;
+    } else {
+        var mainImageName = 'noimage.png';
+    }
+
+    // Form Validation
+
+    req.checkBody('title', 'Title field is required').notEmpty();
+    req.checkBody('body','Body field is required').notEmpty();
+
+    // Check Errors
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('addpost', {
+            "erors": errors,
+            "title": title,
+            "body": body
+        });
+    } else {
+        var posts = db.get('posts');
+
+        // Submit to db
+        posts.insert({
+            "title": title,
+            "body": body,
+            "category": category,
+            "date": date,
+            "author": author,
+            "image": mainImageName
+        }, function(err, post){
+            if(err){
+                res.send('There was an issue submitting the post');
+            } else {
+                req.flash('success', 'Post Submitted');
+                res.location('/');
+                res.redirect('/');
+            }
+        });
+    }
+
+
+});
+module.exports = router;

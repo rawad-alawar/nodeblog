d5e3b40e0f565434c4615a75e7d2cb467d0bd921
Rawad Alawar <email": null>
2016-04-13 12:00:33 +1200

    lets visitors view posts by category

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,16 @@ var router = express.Router();
 var mongo = require('mongodb')
 var db = require('monk')('localhost/nodeblog')   //name of our db is localhost/nodeblog
 
-
+router.get('/show/:category', function(req,res,next){
+  var db = req.db
+  var posts = db.get('posts')
+  posts.find({category: req.params.category},{}, function(err, posts){
+    res.render('index', {
+      "title": req.params.category,
+      "posts": posts
+    })
+  });
+})
 router.get('/add', function(req, res, next) {
   res.render('addcategory', {
     "title": "Add Category"             //passes in a title called Add Category
